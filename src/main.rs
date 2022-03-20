a57a665dd4b2e9d0969ea295f5841c6f8f237f52
Manabu Abe <fr@ssb-notion.com>
2022-03-20 15:21:17 +0900

    The first version.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
 use bevy::prelude::*;
 
-const UNIT_WIDTH: u32 = 40;
-const UNIT_HEIGHT: u32 = 40;
-
-const X_LENGTH: u32 = 10;
-const Y_LENGTH: u32 = 18;
-
-const SCREEN_WIDTH: u32 = UNIT_WIDTH * X_LENGTH;
-const SCREEN_HEIGHT: u32 = UNIT_HEIGHT * Y_LENGTH;
-
 fn main() {
     App::new()
         .insert_resource(WindowDescriptor {
